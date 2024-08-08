0a385d5ff7183643c6d724a7674781febee958f0
Mbaduko <nsengiyumvaclement247@gmail.com>
2024-08-08 12:27:25 +0200

    feat: Dine

--- index.js
+++ index.js
@@ -19,10 +19,16 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get("/api/whoami", (req,res) => {
+  const ipaddress = req.headers['x-forwarded-for'] || req.ip ;
+  const language = req.headers['accept-language'] || "unknown";
+  const software = req.headers['user-agent'] || 'unknown';
+  return res.json({
+    ipaddress,
+    language,
+    software
+  })
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
